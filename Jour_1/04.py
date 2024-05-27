b2f5a6f4206cabdaa93f6cbf38b7c1e25e4f89c0
TimotheeRoy <timroy93100@gmail.com>
2024-05-27 14:10:55 +0200

    add flip without numpy

--- Jour_1/04.py
+++ Jour_1/04.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def flipArr(mat, str):
+def ezFlip(mat, str):
     if str == "horizontal":
         return np.flip(mat,1)
     elif str == "vertical":
@@ -9,10 +9,25 @@ def flipArr(mat, str):
         return "mode incorrecte"
     
 
+def flipArr(arr, str): 
+    rep = []
+    if str == "horizontal":
+        for list in arr :
+            reversed = list[::-1]
+            rep.append(reversed)
+        return rep
+    elif str == "vertical":
+        rep = arr[::-1]
+        return rep
+    else :
+        return "mode incorrecte"
+
+
 array = [
 [1,2,3],
 [4,5,6],
 [7,8,9]
 ]
+
 print(flipArr(array, 'horizontal'))
 print(flipArr(array, 'vertical'))

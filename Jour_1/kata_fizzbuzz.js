e7db7126c7e82cdb98091e00d206abdcb0b9f9cf
julieATS <julie-bonnin97@hotmail.fr>
2024-05-27 10:46:33 +0200

    kata 5 fizzbuzz ok

--- Jour_1/kata_fizzbuzz.js
+++ Jour_1/kata_fizzbuzz.js
@@ -6,13 +6,6 @@ et les multiples de 15 par FizzBuzz.
 */
 
 function fizzBuzz () {
-    /*
-    affiche nombre (comportement par défaut)
-    si multiple de 3, remplace nombre par "Fizz"
-    si multiple de 5, remplace nombre par "Buzz"
-    si multiple de 15, remplace nombre par "FizzBuzz"
-        15 est un multiple de 3 et 5, donc vérifier mult 15 en premier
-    */
     for (let i = 1; i<=100; i++) {
         if (i%15 == 0) {
             console.log("FizzBuzz")

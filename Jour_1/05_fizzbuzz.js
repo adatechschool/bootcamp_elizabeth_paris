4efc2151adc085b4043e0438ee556d5f978532e9
MorganeLM <lets.morganise.it@gmail.com>
2024-05-28 10:31:30 +0200

    exo 5 pseudo code by Bruno

---
+++ Jour_1/05_fizzbuzz.js
@@ -0,0 +1,8 @@
+// Fizzbuzz
+// Rédigez un programme informatique qui affiche les nombres de 1 à 100 dans l’ordre, en remplaçant les multiples de 3 par le mot Fizz, les multiples de 5 par Buzz, et les multiples de 15 par FizzBuzz.
+
+// en pseudo code
+if (n%3 ou n%5) {
+    if(n%5) print Fizz
+    if(n%3) print Buzz
+} else print n

3be7789f385c1c09c1063bb506386c065031e03e
julieATS <julie-bonnin97@hotmail.fr>
2024-05-28 10:19:39 +0200

    created objects respondent, manager and director

---
+++ Jour_2/call_center.js
@@ -0,0 +1,39 @@
+/*
+Créer trois objets :
+respondent
+manager
+director
+*/
+
+const respondent = {
+    name: Marie,
+    status: respondent,
+    isAvailable: true,
+
+    getCall: function () {
+        console.log(`The ${this.status} ${this.name} get the call.`)
+    },
+
+};
+
+const manager = {
+    name: Hortense,
+    status: manager,
+    isAvailable: true,
+
+    getCall: function () {
+        console.log(`The ${this.status} ${this.name} get the call.`)
+    },
+
+};
+
+const director = {
+    name: Pauline,
+    status: director,
+    isAvailable: true,
+
+    getCall: function () {
+        console.log(`The ${this.status} ${this.name} get the call.`)
+    },
+
+};

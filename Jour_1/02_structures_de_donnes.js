14fb3af6e77a34a6cd7aa2b967c24490cad7604e
MorganeLM <lets.morganise.it@gmail.com>
2024-05-28 10:31:10 +0200

    exo 2 review by Bruno

--- Jour_1/02_structures_de_donnes.js
+++ Jour_1/02_structures_de_donnes.js
@@ -5,6 +5,9 @@
 const stringEx1 = "ma super phrase";
 const stringEx2 = "ma super";
 
+// bien poser d'abord les exclusions/questions
+// lower/uppercase sont perçues comme identiques ou non ?
+
 // with extra datastructure "set"
 let uniqueChar = (string) => {
   const charSet = new Set();
@@ -22,6 +25,10 @@ console.log('with set :', uniqueChar(stringEx2));
 
   // without new datastructure
   let uniqueChar2 = (string) => {
+    if (string.length > 128) {
+      // si ça dépasse le nombre de caratères du jeu de caratères (ascii, alphanum), il y a forcément du doublon ! maliiiiiin
+      return true;
+    }
     for (let i = 0; i < string.length; i++) {
       for (let j = i + 1; j < string.length; j++) {
         if (string[i] === string[j]) {
